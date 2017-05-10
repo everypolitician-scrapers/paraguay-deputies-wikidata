58bc2b54db910854e008f09522577b7043c1c85f
Tony Bowden <tony@tmtm.com>
2017-05-10 10:28:33 +0100

    Also fetch via P39s

--- scraper.rb
+++ scraper.rb
@@ -5,4 +5,7 @@ require 'wikidata/fetcher'
 
 names = WikiData::Category.new('Category:Members of the Chamber of Deputies of Paraguay', 'en').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+sparq = 'SELECT ?item WHERE { ?item wdt:P39 wd:Q20058561 . }'
+ids = EveryPolitician::Wikidata.sparql(sparq)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: names })
